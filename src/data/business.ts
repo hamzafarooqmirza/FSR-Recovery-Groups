bf9b4675e19c8a0459ab5153a681d39581e65658
hamzafarooqmirza <hamza.farooq.mirza@gmail.com>
2026-08-26 20:08:02 +0500

    Resolve Jumpstart business data conflict

---
+++ src/data/business.ts
@@ -0,0 +1,13 @@
+export const business = {
+  name: "FSR Recovery Group",
+  phone: "+447888502989",
+  phoneDisplay: "+44 7888 502989",
+  tel: "tel:+447888502989",
+  address: {
+    street: "135 Gladstone St",
+    locality: "Darlington",
+    postalCode: "DL3 6LB",
+    countryCode: "GB",
+  },
+  addressFull: "135 Gladstone St, Darlington DL3 6LB, United Kingdom",
+} as const;
